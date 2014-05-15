1e05d74ed9cb8226ff92246fe94fa03b3d25cdba
natobyte <natobyte@hotmail.com>
2014-05-15 07:52:40 -0300

    Ajuste nos procedimentos jquery do mdulo de menus

--- app/views/admin/menus/create.blade.php
+++ app/views/admin/menus/create.blade.php
@@ -5,7 +5,7 @@
 	<div id="recordMenu" class='container'>
 		<div id="divHeader" class="page-header">
 			<div style="float:left"><h2>Menus</h2></div>
-			<div style="float:right; margin-top:21px;">@include('admin.groups.submenu')</div>
+			<div style="float:right; margin-top:21px;">@include('admin.menus.submenu')</div>
 			<div class="clear"></div>
 		</div>
 		<div>
@@ -13,10 +13,26 @@
 			<div id="alerts"></div>
 			<form id="addMenu" name="addMenu" class="form-horizontal">
 				<fieldset>
+					<!-- Multiple Radios (inline) -->
+					<div class="form-group">
+						<label class="col-md-2 control-label" for="nivel">Nível</label>
+						<div class="col-md-6"> 
+							<label class="radio-inline" for="nivel-0">
+								<input type="radio" name="nivel" id="nivel-0" value="0" checked="checked">Menu
+							</label> 
+							<label class="radio-inline" for="nivel-1">
+								<input type="radio" name="nivel" id="nivel-1" value="1">Sub-menu
+							</label> 
+							<label class="radio-inline" for="nivel-2">
+								<input type="radio" name="nivel" id="nivel-2" value="2">Sub-sub-menu
+							</label>
+						</div>
+					</div>
+
 					<!-- Text input-->
 					<div class="form-group">
 						<label class="col-md-2 control-label" for="name">Menu</label>  
-						<div class="col-md-6">
+						<div id="menu" class="col-md-6">
 							<input id="menu" name="menu" type="text" placeholder="" class="form-control input-md" required="">
 						</div>
 					</div>
@@ -53,8 +69,8 @@
 					<div class="form-group">
 						<label class="col-md-4 control-label" for="btSave"></label>
 						<div class="col-md-4">
-							<buttom type="submit" id="btSave" name="btSave" class="btn btn-info" form="addGroup"><i class="glyphicon glyphicon-ok"></i> Salvar</buttom>
-							<a id="btCancel" name="btCancel" class="btn btn-danger" href="{{{URL::route('groups.index')}}}"><i class="glyphicon glyphicon-remove"></i> Cancelar</a>							
+							<buttom type="submit" id="btSave" name="btSave" class="btn btn-info" form="addMenu"><i class="glyphicon glyphicon-ok"></i> Salvar</buttom>
+							<a id="btCancel" name="btCancel" class="btn btn-danger" href="{{{URL::route('menus.index')}}}"><i class="glyphicon glyphicon-remove"></i> Cancelar</a>							
 						</div>
 					</div>
 				</fieldset>
